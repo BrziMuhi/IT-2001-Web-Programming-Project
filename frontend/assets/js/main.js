8b729d88e7bfab66746111d46e5bdd642231de6d
Your Name <muhamed.sekic@stu.ibu.edu.ba>
2025-10-18 12:09:06 +0200

    Folder Structure and SPAPP

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,174 +1,175 @@
 var app = $.spapp({
-    defaultView: "#hero",
-    templateDir: "./pages/"
-
+  defaultView  : "hero",
+  templateDir  : "./pages/",
+  pageNotFound : "error_404"
 });
+
+app.route({
+  view : "hero",
+  load : "hero.html",
+  onCreate: function() { 
+    console.log("Hero section created Cevapppii");
+   },
+  onReady: function() {
+    console.log("Hero section ready Cevapppii");
+   }
+});
+app.route({ view : "about", load : "about.html" });
+app.route({ view : "destinations", load : "destinations.html" });
+app.route({ view : "call-to-action", load : "call-to-action.html" });   
+app.route({ view : "portfolio", load : "gallery.html" }); 
+app.route({ view : "team", load : "team.html" });
+app.route({ view : "pricing", load : "pricing.html" });  
+app.route({ view : "faq", load : "faq.html" });
+app.route({ view : "contact", load : "contact.html" });
 
 
 app.run();
 
-// (function() {
-//   "use strict";
+(function() {
+  "use strict";
 
-//   /**
-//    * Apply .scrolled class to the body as the page is scrolled down
-//    */
-//   function toggleScrolled() {
-//     const selectBody = document.querySelector('body');
-//     const selectHeader = document.querySelector('#header');
-//     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-//     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-//   }
+  /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
 
-//   document.addEventListener('scroll', toggleScrolled);
-//   window.addEventListener('load', toggleScrolled);
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
 
-//   /**
-//    * Mobile nav toggle
-//    */
-//   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-//   function mobileNavToogle() {
-//     document.querySelector('body').classList.toggle('mobile-nav-active');
-//     mobileNavToggleBtn.classList.toggle('bi-list');
-//     mobileNavToggleBtn.classList.toggle('bi-x');
-//   }
-//   mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavToggleBtn.classList.toggle('bi-list');
+    mobileNavToggleBtn.classList.toggle('bi-x');
+  }
+  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
 
-//   /**
-//    * Hide mobile nav on same-page/hash links
-//    */
-//   document.querySelectorAll('#navmenu a').forEach(navmenu => {
-//     navmenu.addEventListener('click', () => {
-//       if (document.querySelector('.mobile-nav-active')) {
-//         mobileNavToogle();
-//       }
-//     });
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
 
-//   });
+  });
 
-//   /**
-//    * Toggle mobile nav dropdowns
-//    */
-//   document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-//     navmenu.addEventListener('click', function(e) {
-//       e.preventDefault();
-//       this.parentNode.classList.toggle('active');
-//       this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-//       e.stopImmediatePropagation();
-//     });
-//   });
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
 
-//   /**
-//    * Preloader
-//    */
-//   const preloader = document.querySelector('#preloader');
-//   if (preloader) {
-//     window.addEventListener('load', () => {
-//       preloader.remove();
-//     });
-//   }
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
 
-//   /**
-//    * Scroll top button
-//    */
-//   let scrollTop = document.querySelector('.scroll-top');
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
 
-//   function toggleScrollTop() {
-//     if (scrollTop) {
-//       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-//     }
-//   }
-//   scrollTop.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth'
-//     });
-//   });
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
 
-//   window.addEventListener('load', toggleScrollTop);
-//   document.addEventListener('scroll', toggleScrollTop);
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
 
-//   /**
-//    * Animation on scroll function and init
-//    */
-//   function aosInit() {
-//     AOS.init({
-//       duration: 600,
-//       easing: 'ease-in-out',
-//       once: true,
-//       mirror: false
-//     });
-//   }
-//   window.addEventListener('load', aosInit);
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
 
-//   /**
-//    * Init swiper sliders
-//    */
-//   function initSwiper() {
-//     document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
-//       let config = JSON.parse(
-//         swiperElement.querySelector(".swiper-config").innerHTML.trim()
-//       );
 
-//       if (swiperElement.classList.contains("swiper-tab")) {
-//         initSwiperWithCustomPagination(swiperElement, config);
-//       } else {
-//         new Swiper(swiperElement, config);
-//       }
-//     });
-//   }
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
 
-//   window.addEventListener("load", initSwiper);
+  /**
+   * Init isotope layout and filters
+   */
+  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
 
-//   /**
-//    * Initiate glightbox
-//    */
-//   const glightbox = GLightbox({
-//     selector: '.glightbox'
-//   });
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+        itemSelector: '.isotope-item',
+        layoutMode: layout,
+        filter: filter,
+        sortBy: sort
+      });
+    });
 
-//   /**
-//    * Init isotope layout and filters
-//    */
-//   document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
-//     let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-//     let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-//     let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+      filters.addEventListener('click', function() {
+        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        initIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aosInit === 'function') {
+          aosInit();
+        }
+      }, false);
+    });
 
-//     let initIsotope;
-//     imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
-//       initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-//         itemSelector: '.isotope-item',
-//         layoutMode: layout,
-//         filter: filter,
-//         sortBy: sort
-//       });
-//     });
+  });
 
-//     isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-//       filters.addEventListener('click', function() {
-//         isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-//         this.classList.add('filter-active');
-//         initIsotope.arrange({
-//           filter: this.getAttribute('data-filter')
-//         });
-//         if (typeof aosInit === 'function') {
-//           aosInit();
-//         }
-//       }, false);
-//     });
+  /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
 
-//   });
-
-//   /**
-//    * Frequently Asked Questions Toggle
-//    */
-//   document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-//     faqItem.addEventListener('click', () => {
-//       faqItem.parentNode.classList.toggle('faq-active');
-//     });
-//   });
-
-// })();
+})();
